98d070e8457ec5374c86ffe50e7febafd3b36196
Juan Jesus Padrón Hernández <jpadron@opencanarias.es>
2020-11-02 14:04:59 +0000

    Bot blockeador

---
+++ main.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+
+from twitter_bot import TwitterBot
+from os import environ
+import utils
+
+API_KEY = utils.getenv('API_KEY','')
+API_KEY_SECRET = utils.getenv('API_KEY_SECRET','')
+ACCESS_TOKEN = utils.getenv('ACCESS_TOKEN','')
+ACCESS_TOKEN_SECRET = utils.getenv('ACCESS_TOKEN_SECRET','')
+STALKER_ACCOUNT = utils.getenv('STALKER_ACCOUNT','')
+ACCOUNT = utils.getenv('ACCOUNT', '')
+
+def check_credentials(credentials):
+    not_empty_items = list( filter(lambda item: item != '', credentials))
+    return len(not_empty_items) == len(credentials)
+
+if __name__ == "__main__":
+    credentials = {
+        'api_key': API_KEY,
+        'api_key_secret':API_KEY_SECRET,
+        'access_token':ACCESS_TOKEN,
+        'access_token_secret':ACCESS_TOKEN_SECRET
+    }
+
+
+    if check_credentials(credentials) & (STALKER_ACCOUNT != ''):
+        bot = TwitterBot(credentials, ACCOUNT,STALKER_ACCOUNT)
+        # bot.block_followersOf_replying(ACCOUNT, STALKER_ACCOUNT)
+        bot.block_followersOf_replying()
+    else:
+        print('Credenciales no validas')
---
+++ twitter_bot.py
@@ -0,0 +1,65 @@
+from TwitterAPI import TwitterAPI
+from text_generator import TextGenerator
+import json
+
+class TwitterBot:
+    def __init__(self, credentials, account, stalker_account):
+        self.api = TwitterAPI(credentials['api_key'], 
+                    credentials['api_key_secret'],
+                    credentials['access_token'],
+                    credentials['access_token_secret'])
+        
+        self.account = account
+        self.text_generator = TextGenerator()
+        self.stalker_account = stalker_account
+        self.stalker_account_id = self.__get_stalker_account_id()
+    
+    def block_followersOf_replying(self):
+        stream = self.api.request('statuses/filter', { 'follow': self.account })
+        for tweet in stream:
+            # print(tweet)
+            if self.__user_follows_stalker(tweet):
+                user = tweet['user']['id']
+                self.api.request('blocks/create', { 'user_id' : user })
+                print(f'User @{user} blocked!')
+            # if is_following_stalker():
+
+
+    def __get_stalker_account_id(self):
+        response = self.api.request('users/show', {'screen_name':self.stalker_account})
+
+        if response.status_code == 200:
+            return  json.loads(response.text)['id_str']
+        else:
+            print('PROBLEM: ' + response.text)
+            exit()
+
+    def __user_follows_stalker(self, tweet):
+        if 'user' in tweet:
+            user = tweet['user']['screen_name']
+            relation = self.api.request('friendships/show', { 'source_screen_name': user, 'target_screen_name': self.stalker_account })
+
+            content = json.loads(relation.response._content)
+
+            # print(content['relationship']['target']['followed_by'])
+            return content['relationship']['target']['followed_by']
+
+        else:
+            return False
+
+    def __tweeted_by_stalked_account(self, tweet):
+        return (tweet['text'][0:4] == 'RT @')
+
+
+    # def __tweeted_by_stalked_account(self, tweet):
+    #     return (tweet['text'][0:4] != 'RT @')
+
+    # def __reply(self, tweet_id):
+    #     tweet_text = self.text_generator.get_random_text()
+    #     response = self.api.request('statuses/update', {'status': f'@{self.stalked_account} {tweet_text}', 'in_reply_to_status_id': tweet_id})
+    #     print('SUCCESS' if response.status_code == 200 else 'PROBLEM: ' + response.text)
+
+    # def __fav(self, tweet_id):
+    #     response = self.api.request('favorites/create', {'id': tweet_id})
+    #     print('SUCCESS' if response.status_code == 200 else 'PROBLEM: ' + response.text)
+
